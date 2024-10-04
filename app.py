aa4cacb49b4837bc3bc8a76fea91cf02dfbad721
duziy <106121537+realduziy@users.noreply.github.com>
2024-10-04 11:19:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
+    name = "duziy"  # Replace with the actual name
+    statuses = ["Music is life!", "Peace and love!"]  # Your status messages
+    return render_template('index.html', name=name, statuses=statuses)
 
 if __name__ == "__main__":
     app.run(debug=True)
