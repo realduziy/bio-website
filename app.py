7799ee15a31dcfa4ac736c4d30c981d865c6a2cc
duziy <106121537+realduziy@users.noreply.github.com>
2024-10-04 00:05:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template("index.html")  # Render the index.html template
+    return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 
 if __name__ == "__main__":
-    app.run(debug=True)  # Enable debug mode for development
+    app.run(debug=True)
