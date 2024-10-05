b54dc05d9dcceb739e3b47b27ad25c58a5813286
duziy <106121537+realduziy@users.noreply.github.com>
2024-10-05 10:54:17 -0500

    removed debug

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     name = "duziy"  # Replace with the actual name
     statuses = ["Music is life!", "Peace and love!"]  # Your status messages
-    return render_template('index.html', name=name, statuses=statuses)
+    return render_template("index.html", name=name, statuses=statuses)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
